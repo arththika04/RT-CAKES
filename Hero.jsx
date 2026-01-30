b209f0d302f9da2e37ea56ab2e7b3bad4d72d6e0
Aarthika <aarthika@gmail.com>
2026-01-30 09:03:03 +0530

    done

---
+++ Hero.jsx
@@ -0,0 +1,16 @@
+// Hero.jsx
+import './style.css';
+
+export default function Hero() {
+  return (
+    <>
+      <nav className="navbar">
+        {/* nav links ... */}
+      </nav>
+
+      <section className="hero">
+        {/* un hero HTML content same */}
+      </section>
+    </>
+  );
+}
