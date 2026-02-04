c71d3378da5943a92e5fdcd7de89829b340b0f30
Aarthika <arththika04@gmail.com>
2026-02-04 10:41:26 +0530

    first commit

--- About.jsx
+++ About.jsx
@@ -12,3 +12,15 @@ export default function About() {
         <p className="about-tagline">
           <em>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis 
           suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur uis autem vel eum.</em>
+           </p>
+        <p className="about-text">
+          Nor again is there anyone who loves or pursues or desires to obtain pain of 
+          itself, because it is pain, but because occasionally circumstances occur in 
+          which toil and pain can procure him some great pleasure. To take a trivial 
+          example, which of us ever undertakes laborious physical exercise.
+        </p>
+        <button className="about-btn">Learn More</button>
+      </div>
+    </section>
+  );
+}
