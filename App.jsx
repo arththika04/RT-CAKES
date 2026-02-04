81a045ab4e93e74df5148279ec60c3eaaffed4ab
Aarthika <arththika04@gmail.com>
2026-02-04 10:50:46 +0530

    first commit

--- App.jsx
+++ App.jsx
@@ -6,3 +6,17 @@ import About from './About';
 function Home() {
   return <Hero />;
 }
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<div>Service Page</div>} />
+        <Route path="/products" element={<div>Products Page</div>} />
+        <Route path="/contact" element={<div>Contact Page</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
