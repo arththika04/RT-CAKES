f198b57fde769c929849867ca86e03c28aeda5bb
Aarthika <arththika04@gmail.com>
2026-02-04 10:51:33 +0530

    first commit

---
+++ main.jsx
@@ -0,0 +1,11 @@
+// main.jsx (Vite) or index.jsx (CRA)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
