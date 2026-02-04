6c82b5b7f12496301afae52dab0ef7052b8fba7d
Aarthika <arththika04@gmail.com>
2026-02-04 10:49:17 +0530

    first commit

--- Navbar.jsx
+++ Navbar.jsx
@@ -7,3 +7,11 @@ export default function Navbar() {
       <ul className="nav-links">
         <li><Link to="/" className="active">HOME</Link></li>
         <li><Link to="/service">SERVICE</Link></li>
+         <li><Link to="/products">PRODUCTS</Link></li>
+        <li><Link to="/about">ABOUT US</Link></li>
+        <li><Link to="/contact">CONTACT US</Link></li>
+      </ul>
+      <button className="nav-btn">SHOP NOW</button>
+    </nav>
+  );
+}
