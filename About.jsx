768b24dec3dc87378c7739c60e79dbd7d13ec612
Aarthika <arththika04@gmail.com>
2026-02-04 10:40:59 +0530

    first commit

---
+++ About.jsx
@@ -0,0 +1,14 @@
+import './About.css';
+
+export default function About() {
+  return (
+    <section className="about">
+      <div className="about-image-wrapper">
+        <img src="cupcakes.jpg" alt="Delicious cupcakes" className="about-image" />
+      </div>
+      <div className="about-content">
+        <h4 className="about-subtitle">ABOUT US</h4>
+        <h2 className="about-title">Welcome to our Bakery</h2>
+        <p className="about-tagline">
+          <em>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis 
+          suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur uis autem vel eum.</em>
